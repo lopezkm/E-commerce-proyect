79f8b2c26f2a26de18d85de1a1723086faa7b2f2
Kevin-V352 <67164849+Kevin-V352@users.noreply.github.com>
2020-10-07 14:32:04 -0300

    DONE - S8 : Crear Formulario para el CRUD de un Producto

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import './App.css';
-import SearchBar from './components/search_bar.jsx'
+import SearchBar from './components/search_bar.jsx';
 import Product from './components/product.jsx';
+import FormAdmin from './components/form_admin.jsx';
 import IMG from './punisher.jpg';
 
 function App() {
@@ -19,6 +20,8 @@ function App() {
       publisher= {'Nintendo'}
       publishDate= {'17 de Octubre de 2004'}
       />
+      <br/>
+      <FormAdmin/>
     </div>
   );
 }
---
+++ client/src/components/form_admin.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Nav, Form, Button, Navbar, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const FormAdmin = () => {
+    return (
+        <div>
+            {/* Opciones para CRUD del producto */}
+            <Nav variant="pills">
+                <Nav.Item>
+                    <Nav.Link href="/Admin/Crear">Crear</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/Admin/Modificar">Modificar</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/Admin/Borrar">Borrar</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <br/>
+
+            {/* Barra de busqueda del producto (para borrar y modificar) */}
+            <Navbar>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+                    <Button type="submit">Submit</Button>
+                </Form>
+            </Navbar>
+            <br/>
+
+            {/* Formulario para modificar o crear el producto */}
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control type="text" placeholder="Ingrese el nombre de su videojuego..." />
+                    <br/>
+                    <Form.Label>Descripcion</Form.Label>
+                    <Form.Control type="text" placeholder="Ingrese la descripcion para su videojuego..." />
+                    <br/>
+                    <Form.Label>Stock disponible</Form.Label>
+                    <Form.Control type="number" placeholder="Cantidad disponible..."/>
+                    <br/>
+                    <Form.Label>Desarrollado por:</Form.Label>
+                    <Form.Control type="text" placeholder="Desarrolladora"/>
+                    <br/>
+                    <Form.Label>Publicado por:</Form.Label>
+                    <Form.Control type="text" placeholder="Publicadora" />
+                    <br/>
+                    <Form.Label>Fecha de lanzamiento:</Form.Label>
+                    <Form.Control type="date" placeholder="Fecha de lanzamiento" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default FormAdmin;
