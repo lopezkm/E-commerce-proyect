4f8dddf88ac1e6de2161b9c82820fea63e6e2873
FrancoBonomo <56139775+FrancoBonomo@users.noreply.github.com>
2020-10-07 16:21:45 -0300

    Fix boostrap

--- client/src/components/product_card.jsx
+++ client/src/components/product_card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ProductCard = ({ name, price, media, developer}) => {
     return(
