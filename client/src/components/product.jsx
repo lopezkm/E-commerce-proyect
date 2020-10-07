28018e4abfefc6ca28a21dd761799780c7e98c78
Santiago <santiagojaviermartinez@gmail.com>
2020-10-07 14:54:41 -0300

    S6 mejoras en el codigo

--- client/src/components/product.jsx
+++ client/src/components/product.jsx
@@ -56,23 +56,17 @@ const Product = ({ name, description, price, stock, media, developer, publisher,
 
             <Card style={{ width: '100%' }}>
                 <Card.Body>
-                    <div className="text-center"> <Card.Title > TITULO DEL JUEGO: {name} </Card.Title> </div>
-                    <div>
+                     <Card.Title className="text-center"> TITULO DEL JUEGO: {name} </Card.Title>
                         <Card.Text >
                             
                             <p className="text-center"> ESTA ES UNA DESCRIPCIÓN DEL JUEGO: {description} </p>
-                            <span> Stock disponible: {stock} </span>
-                            <br />
-                            <span>Desarrollado por: {developer}</span>
-                            <br />
-                            <br />
-                            <br />
-                           <div className= "d-flex">
-                            <span>Publicado por: {publisher}  |  Fecha de publicación: {publishDate} </span>
-                            <Button className={"d-flex ml-auto"}>ADD for $ {price} </Button>
+                            <p> Stock disponible: {stock} </p>
+                            <p> Desarrollado por: {developer}   </p>
+                            <div className= "d-flex">
+                                <p>Publicado por: {publisher}  |  Fecha de publicación: {publishDate} </p>
+                                <Button className="d-flex ml-auto">ADD for $ {price} </Button>
                             </div>
                         </Card.Text>
-                    </div>
                 </Card.Body>
             </Card>
         </div>
