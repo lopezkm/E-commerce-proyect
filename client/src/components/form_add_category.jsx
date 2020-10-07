fc5ab508338f737d3d7e29699527b21b93e01d03
Carlos Saballe <ing.carlossaballe@gmail.com>
2020-10-07 15:20:37 -0500

    DONE - S9 : Crear Formulario para creación de Categorías - Description Added

--- client/src/components/form_add_category.jsx
+++ client/src/components/form_add_category.jsx
@@ -8,9 +8,11 @@ const FormAddCategory = () => {
             {/* Formulario para agregar categorías */}
             <h1>Agregar nueva categoría</h1>
             <Form>
-                <Form.Group controlId="formBasicEmail">
+                <Form.Group >
                     <Form.Label>Nombre de la categoria</Form.Label>
-                    <Form.Control type="text" placeholder="Ingrese nombre de categoría..." />
+                    <Form.Control type="text" placeholder="Ingresa nombre de categoría..." />
+                    <Form.Label> Descripción</Form.Label>
+                    <Form.Control type="text" placeholder="Agrega una breve descripción..." />
                 </Form.Group>
 
                 <Button variant="primary" type="submit"> Agregar </Button>
