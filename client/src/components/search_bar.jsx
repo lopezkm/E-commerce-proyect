1fc43587648b01626cf5b9fe4cf5f266bb6db868
Kevin-V352 <67164849+Kevin-V352@users.noreply.github.com>
2020-10-07 13:03:36 -0300

    DONE - S7 : Crear Componente Search Bar

---
+++ client/src/components/search_bar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Form, Button, FormControl, Navbar } from 'react-bootstrap'
+
+const SearchBar = () => {
+    const [input, setInput] = React.useState({
+        searchInput: "",
+    });
+
+    const handleInputChange = (event) =>{
+        setInput({
+          ...input,
+          [event.target.name] : event.target.value
+        });
+    };
+
+    return (
+        <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl 
+        type="text" 
+        placeholder="Search your game" 
+        className="mr-sm-2"  
+        name="searchInput" 
+        onChange={handleInputChange}/>
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+    );
+};
+
+export default SearchBar;
