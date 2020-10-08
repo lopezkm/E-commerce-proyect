6d28891a2360229b0f0102efd9a63ac201023301
Santiago <santiagojaviermartinez@gmail.com>
2020-10-08 00:25:23 -0300

    S19 terminada. Agregamos la ruta DELETE Category en category.js. Agregamos router.use de category en index.js

---
+++ api/src/routes/category.js
@@ -0,0 +1,18 @@
+const server = require('express').Router();
+const { Category } = require('../db.js');
+
+
+server.delete("/:id", (req, res) => {
+    let { id } = req.params
+    Category.findByPk(id)
+        .then(category => {
+            if (!category) {
+                return res.sendStatus(404)
+            }
+
+            category.destroy()
+            res.sendStatus(200)
+        })
+})
+
+module.exports = server;
