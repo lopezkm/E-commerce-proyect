c401b956dc328c38a89ef21779442a3e243b01a2
FrancoBonomo <56139775+FrancoBonomo@users.noreply.github.com>
2020-10-08 01:32:00 -0300

    Solved S18

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -1,18 +1,28 @@
 const server = require('express').Router();
 const { Category } = require('../db.js');
 
+server.post("/", (req, res) => {
+    let {name, description} = req.body;
+    console.log(req.body);
+    name = name.toUpperCase();
+    Category.create({
+        name: name,
+        description: description 
+    }).then(() => res.sendStatus(200))
+    .catch(() => res.status(409).send("La categoría a crear ya existe."));
+});
 
 server.delete("/:id", (req, res) => {
-    let { id } = req.params
+    let { id } = req.params;
     Category.findByPk(id)
         .then(category => {
             if (!category) {
-                return res.sendStatus(404)
+                return res.status(404).send("La categoría a eliminar no existe.");
             }
 
             category.destroy()
             .then(() => res.sendStatus(200));
-        })
-})
+        });
+});
 
 module.exports = server;
