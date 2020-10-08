b1f47db7aafc0e2b15ca4033915318f8c25b586e
manu96lp <manu96_lp@hotmail.com>
2020-10-08 02:01:02 -0300

    DONE - S17: Crear ruta para agregar o sacar categorías de un producto

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,8 @@
 const { Router } = require('express');
-// import all routers;
+
 const productRouter = require('./product.js');
+const router = Router( );
 
-
-const router = Router();
-
-// load each router on a route
-// i.e: router.use('/auth', authRouter);
-// router.use('/auth', authRouter);
-router.use('/products', productRouter);
+router.use( '/products', productRouter );
 
 module.exports = router;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,54 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
+const server = require( 'express' ).Router( );
+const { Product, Category } = require( '../db.js' );
+const Promise = require( 'bluebird' );
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
-});
+server.get( '/', ( request, response, next ) => {
+	Product.findAll( )
+		.then( ( products ) => {
+			response.send( products );
+		} );
+} );
+
+server.post( '/:idProduct/category/:idCategory', ( request, response, next ) => {
+	const { idProduct, idCategory } = request.params;
+	const promises = [ ];
+	
+	promises.push( Product.findByPk( idProduct ) );
+	promises.push( Category.findByPk( idCategory ) );
+	
+	Promise.all( promises )
+		.spread( function( product, category ) {
+			if ( !product || !category ) {
+				return response.sendStatus( 404 );
+			}
+			
+			product.addCategory( category )
+				.then( ( data ) => {
+					( !!data ) ?
+						response.sendStatus( 201 ) : response.sendStatus( 409 );
+				} );
+		} );
+} );
+
+server.delete( '/:idProduct/category/:idCategory', ( request, response, next ) => {
+	const { idProduct, idCategory } = request.params;
+	const promises = [ ];
+	
+	promises.push( Product.findByPk( idProduct ) );
+	promises.push( Category.findByPk( idCategory ) );
+	
+	Promise.all( promises )
+		.spread( function( product, category ) {
+			if ( !product || !category ) {
+				return response.sendStatus( 404 );
+			}
+			
+			product.removeCategory( category )
+				.then( ( data ) => {
+					( !!data ) ?
+						response.sendStatus( 204 ) : response.sendStatus( 409 );
+				} );
+		} );
+} );
 
 module.exports = server;
