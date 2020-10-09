57b300230c869124013e71bc73ba4460c9c1476c
manu96lp <manu96_lp@hotmail.com>
2020-10-09 20:18:39 -0300

    Minor general changes

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,8 +1,5 @@
 const { DataTypes } = require( 'sequelize' );
 
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-
 module.exports = ( sequelize ) => {
 	sequelize.define( 'product', {
 		name: {
