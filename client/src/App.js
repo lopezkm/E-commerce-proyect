1b3e9a5fa64cdc5a032f3fe12f53a35276579f08
manu96lp <manu96_lp@hotmail.com>
2020-10-12 04:03:57 -0300

    Solved import (file realocated) and VS Code merge issues (invisible lines)

--- client/src/App.js
+++ client/src/App.js
@@ -8,28 +8,7 @@ import './App.scss';
 import Product from './components/product.jsx';
 import NavBar from './components/nav_bar.jsx';
 import FormAdmin from './components/form_admin.jsx';
-<<<<<<< HEAD
-import Catalogue from './components/Catalogue/Catalogue.jsx'
-import FormAdd from './components/form_add_category.jsx'
-import Result from './components/search_results.jsx'
-
-function App( )
-{
-	const [products, setProducts] = useState({});
-	const [redirect, setRedirect] = useState({
-		validate: false
-	})
-
-	const findProducts = ( product ) => {
-        axios.get( `http://localhost:3000/search?query=${product}` )
-			.then( response => {
-				setProducts(response);
-		});	
-	};
-
-=======
-import { Route, Redirect } from 'react-router-dom';
-import Catalogue from './components/catalogue.jsx'
+import Catalogue from './components/Catalogue/catalogue.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import FormAdd from './components/form_add_category.jsx'
 import Result from './components/search_results.jsx'
@@ -37,7 +16,6 @@ import Result from './components/search_results.jsx'
 
 function App() {
 	
->>>>>>> master
 	return (
 		<div className= 'container-fluid' style={{ padding: 0}}>
 			<Route path="/" component={() => <NavBar/>} />
