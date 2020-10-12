eef323088002155c706769636e9c3c8c5004f6ad
manu96lp <manu96_lp@hotmail.com>
2020-10-12 04:22:47 -0300

    Changed route catalogue to products (to match stories)

--- client/src/components/nav_bar.jsx
+++ client/src/components/nav_bar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Navbar.Brand href="/">Toni Games</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="/admin">Admin</Nav.Link>
-        <Nav.Link href="/catalogue">Catalogo</Nav.Link>
+        <Nav.Link href="/products">Catalogo</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
       </Nav>
       <SearchBar/>
