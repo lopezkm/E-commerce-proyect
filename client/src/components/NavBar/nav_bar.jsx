58ce70e222fdd31f9b900c2fb12f60306df688cd
Matias Lopez Klyver <lopezk_matias@hotmail.com>
2020-10-13 14:32:09 -0300

    link en navbar para acceder a fomr categories

--- client/src/components/NavBar/nav_bar.jsx
+++ client/src/components/NavBar/nav_bar.jsx
@@ -5,11 +5,12 @@ import SearchBar from '../SearchBar/search_bar.jsx';
 const NavBar = () => {
   return (
     <Navbar expand="md" bg="dark" variant="dark" sticky="top" className="navBar-container">
-      <Navbar.Brand href="/">Toni Games</Navbar.Brand>
+      <Navbar.Brand href="/">Six Games</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto navBar-options">
-          <Nav.Link href="/admin/create">Admin</Nav.Link>
+          <Nav.Link href="/admin/create">Admin-Producto</Nav.Link>
+          <Nav.Link href="/categories">Admin-Categoria</Nav.Link>
           <Nav.Link href="/products">Catalogo</Nav.Link>
         </Nav>
         <SearchBar />
