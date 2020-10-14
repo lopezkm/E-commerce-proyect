7d4000c9079bc6c95d068e7e42be343418e5a58b
Santiago <santiagojaviermartinez@gmail.com>
2020-10-14 16:56:47 -0300

    optimizacion del codigo para mostrar "SIN STOCK" en catalogo

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -6,51 +6,35 @@ function ProductCard({ name, price, media, developer, stock }) {
 		return price.toLocaleString('es-ES', { minimumFractionDigits: 2 });
 	}
 
-	if (stock > 0) {
-		return (
+	return (
 
-			<Card bsPrefix='productCard-card'>
-				<Card.Header bsPrefix='productCard-card-header'>
-					<Card.Img bsPrefix='productCard-card-img' src={media || 'https://i.imgur.com/X7HDtwN.jpg'} alt={`img Game ${name}`} />
-				</Card.Header>
-				<Card.Body bsPrefix='productCard-card-body'>
-					<div>
-						<OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{name}</Tooltip>}>
-							<p className='productCard-card-name'>{name}</p>
-						</OverlayTrigger>
+		<Card bsPrefix='productCard-card'>
+			<Card.Header bsPrefix='productCard-card-header'>
+				<Card.Img bsPrefix='productCard-card-img' src={media || 'https://i.imgur.com/X7HDtwN.jpg'} alt={`img Game ${name}`} />
+			</Card.Header>
+			<Card.Body bsPrefix='productCard-card-body'>
+				<div>
+					<OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{name}</Tooltip>}>
+						<p className='productCard-card-name'>{name}</p>
+					</OverlayTrigger>
 
-						<p className='productCard-card-developer'>{developer}</p>
-					</div>
-
+					<p className='productCard-card-developer'>{developer}</p>
+				</div>
+				
+				{stock !== 0 &&
 					<div>
 						<p className='productCard-card-price'>{getProductPrice()} US$</p>
 					</div>
-				</Card.Body>
-			</Card>
-		)
-	} else {
-		return (
+				}
 
-			<Card bsPrefix='productCard-card'>
-				<Card.Header bsPrefix='productCard-card-header'>
-					<Card.Img bsPrefix='productCard-card-img' src={media || 'https://i.imgur.com/X7HDtwN.jpg'} alt={`img Game ${name}`} />
-				</Card.Header>
-				<Card.Body bsPrefix='productCard-card-body'>
+				{stock === 0 &&
 					<div>
-						<OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{name}</Tooltip>}>
-							<p className='productCard-card-name'>{name}</p>
-						</OverlayTrigger>
-
-						<p className='productCard-card-developer'>{developer}</p>
+						<p className='productCard-card-noStock'>SIN STOCK</p>
 					</div>
-
-					<div className='productCard-card-noStock'>
-						SIN STOCK
-					</div>
-				</Card.Body>
-			</Card>
-		)
-	}
+				}
+			</Card.Body>
+		</Card>
+	)
 }
 
 export default ProductCard;
