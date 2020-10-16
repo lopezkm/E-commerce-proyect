fcefb5952c0b538e10391b52a86e1993d5e0099b
manu96lp <manu96_lp@hotmail.com>
2020-10-16 06:10:23 -0300

    Changed the way we compose enhancers

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -2,14 +2,11 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/reducers.js";
 import thunk from "redux-thunk";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk), 
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ),
-    
+	rootReducer,
+	composeEnhancers( applyMiddleware( thunk ) )
 );
 
 export default store;
