8d4e8a6964a9059da08684b1b1edc390aa884229
Carlos Saballe <ing.carlossaballe@gmail.com>
2020-10-18 12:59:02 -0500

    cart button added to Product.jsx and ProductCard.jsx

---
+++ client/src/components/CartButton.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap'
+
+const AddToCartButton = () => {
+
+
+
+    return (
+        <div>
+
+            <Button variant="success">Agregar al carrito</Button>{' '}
+
+        </div>
+    )
+};
+
+export default AddToCartButton;
