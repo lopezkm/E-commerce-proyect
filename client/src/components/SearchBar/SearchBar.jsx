b336beb3a72e6b5449231867bfa0c4147568a5e9
manu96lp <manu96_lp@hotmail.com>
2020-10-18 16:51:39 -0300

    Modified SearchBar to work with Catalogue, remember its content, and be able to expand itself on use

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,33 +1,91 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useLocation, useHistory } from 'react-router-dom';
+import qs from 'query-string';
 
 function SearchBar( )
 {
+	const [ input, setInput ] = useState( null );
+	
+	const inputWrapper = useRef( null );
+	const inputElement = useRef( null );
+	
+	const location = useLocation( );
 	const history = useHistory( );
-	const [ input, setInput ] = useState( '' );
-
+	
+	const updateHistory = ( ) => {
+		let query = qs.parse( location.search, { arrayFormat: 'comma' } );
+		
+		query.query = input || null;
+		query = qs.stringify( query, { arrayFormat: 'comma' } );
+		
+		history.push( `/products?${ query }` );
+	};
+	
 	const handleSubmit = ( e ) => {
 		e.preventDefault( );
 		
-		if ( input !== '' )
-		{
-			history.push( `/products?query=${ input }` );
-			setInput( '' );
-		}
+		updateHistory( );
 	};
+	
+	const handleInputBlur = ( e ) => {
+		if ( !input ) {
+			inputWrapper.current.style.display = null;
+		}
+	}
+	
+	const handleCloseClick = ( e ) => {
+		setInput( '' );
+		updateHistory( '' );
+		
+		inputWrapper.current.style.display = null;
+	};
+	
+	const handleSearchClick = ( e ) => {
+		if ( input || ( window.innerWidth < 768 ) ) {
+			return;
+		}
+		
+		e.preventDefault( );
+		
+		if ( !inputWrapper.current.style.display ) {
+			inputWrapper.current.style.display = 'block';
+			inputElement.current.focus( );
+		}
+		else {
+			inputWrapper.current.style.display = null;
+		}
+	}
 
 	const handleInputChange = ( e ) => {
 		setInput( e.target.value );
 	};
 	
+	useLayoutEffect( ( ) => {
+		const { query } = qs.parse( location.search, { arrayFormat: 'comma' } );
+		
+		if ( !query ) {
+			return;
+		}
+		
+		setInput( query );
+		
+		inputWrapper.current.style.display = 'block';
+	}, [ ] );
+	
 	return (
 		<form className="searchBar" onSubmit={ handleSubmit }>
-			<button type="submit" className="searchBar__button">
+			<button type="submit" className="searchBar__submit-button" onClick={ handleSearchClick }>
 				<FontAwesomeIcon icon={ faSearch } />
 			</button>
-			<input type="text" value={ input } placeholder="Busca un juego..." name="search" autoComplete="off" className="searchBar__input" onChange={ handleInputChange }/>
+			
+			<div ref={ inputWrapper } className="searchBar__input-wrapper">
+				<input ref={ inputElement } type="text" value={ input } placeholder="Busca un juego..." name="search" autoComplete="off" className="searchBar__input" onBlur={ handleInputBlur } onChange={ handleInputChange }/>
+				<button className="searchBar__close-button" onClick={ handleCloseClick }>
+					<FontAwesomeIcon icon={ faTimes } />
+				</button>
+			</div>
 		</form>
 	);
 }
