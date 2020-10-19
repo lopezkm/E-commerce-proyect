ee9032f66ec1692d426d954e658c9dac13aef4e4
manu96lp <manu96_lp@hotmail.com>
2020-10-19 10:58:38 -0300

    Created ADD_CATEGORY action (REDUX)

--- client/src/redux/action-creators/category.js
+++ client/src/redux/action-creators/category.js
@@ -3,6 +3,29 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+export function addCategory( name, description )
+{
+	return function( dispatch ) {
+		axios.post( `${ API_URL }/products/category`, {
+			name, description
+		} )
+		.then( ( response ) => {
+			dispatch( {
+				type: actionTypes.ADD_CATEGORY,
+				payload: response.data,
+				error: null
+			} );
+		} )
+		.catch( ( error ) => {
+			dispatch( {
+				type: actionTypes.ADD_CATEGORY,
+				payload: null,
+				error: error
+			} );
+		} );
+	};
+}
+
 export function getCategories( )
 {
 	return function( dispatch ) {
--- client/src/redux/reducers/category.js
+++ client/src/redux/reducers/category.js
@@ -9,6 +9,14 @@ function reducer( state = initialState, action )
 {
 	switch ( action.type )
 	{
+		case actionTypes.ADD_CATEGORY:
+		
+			return {
+				...state,
+				categories: [ ...state.categories, action.payload ],
+				error: action.error
+			};
+		
 		case actionTypes.GET_CATEGORIES:
 			
 			return {
