b81d8cab0e8420181b452415134e8d946d6947e2
manu96lp <manu96_lp@hotmail.com>
2020-10-20 02:00:24 -0300

    Small changes to the store

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducers";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index.js';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
