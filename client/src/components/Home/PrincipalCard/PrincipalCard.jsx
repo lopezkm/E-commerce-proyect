61014c4123fb9f2fd07db8cb44c0f2e3dcc30d83
Kevin-V352 <kevinvega2070@gmail.com>
2020-10-21 14:21:16 -0300

    Fixed top frame scrolling overflow.

--- client/src/components/Home/PrincipalCard/PrincipalCard.jsx
+++ client/src/components/Home/PrincipalCard/PrincipalCard.jsx
@@ -5,15 +5,21 @@ const PrincipalCard = () => {
     return (
         <Card className='principalCard-container'>
             <Row>
-                <Col lg={7}>
+                {/* <Col lg={7}>
                     <div className='hola'>
                         <Card.Img src="https://wallpapercave.com/wp/wp1897911.jpg"></Card.Img>
                     </div>
                 </Col>
                 <Col>
                     <Card.Title>Horizon Zero Down</Card.Title>
-                    <Card.Text>Descripcion</Card.Text>
-                </Col>
+                    <Card.Text>Horizon Zero Dawn es un estimulante y nuevo juego de acción y de rol exclusivo para el sistema PlayStation® 4, desarrollado por la galardonada empresa Guerrilla Games, creadores de la venerada franquicia Killzone de PlayStation. </Card.Text>
+                </Col> */}
+                    <Col>
+                        <Card.Img src="https://wallpapercave.com/wp/wp1897911.jpg" />
+                    </Col>
+                    <Col>
+                        <Card.Img src="https://wallpaperset.com/w/full/2/1/2/453698.jpg" />
+                    </Col>
             </Row>
         </Card>
     );
