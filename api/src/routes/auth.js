1f9a53dcd2253ff02e4e765c45fba486bedddfdd
manu96lp <manu96_lp@hotmail.com>
2020-10-23 11:07:53 -0300

    Added </me> route and imported needed things from passport

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,12 +1,23 @@
 const server = require( 'express' ).Router( );
 const passport = require( 'passport' );
 
+const { isAuthenticated, hasAccessLevel, ACCESS_LEVELS } = require( '../passport.js' );
+const { ACCESS_LEVEL_USER, ACCESS_LEVEL_ADMIN, ACCESS_LEVEL_SUPER } = ACCESS_LEVELS;
+
 /* =================================================================================
 * 		[ Identifica un usuario con su cuenta ]
 * ================================================================================= */
 
 server.post( '/login', passport.authenticate( 'local' ), ( request, response, next ) => {
 	response.send( request.user );
+} );
+
+/* =================================================================================
+* 		[ Devuelve el usuario logueado o 401 en caso contrario ]
+* ================================================================================= */
+
+server.get( '/me', isAuthenticated, ( request, response, next ) => {
+	response.status( 200 ).send( request.user );
 } );
 
 /* =================================================================================
