584598f299e2ee6ecc68f54fab5a62d9ecff85f9
manu96lp <manu96_lp@hotmail.com>
2020-10-23 11:09:55 -0300

    Added </signup> route (its probably better that it belongs to auth instead of users)

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -5,6 +5,45 @@ const { isAuthenticated, hasAccessLevel, ACCESS_LEVELS } = require( '../passport
 const { ACCESS_LEVEL_USER, ACCESS_LEVEL_ADMIN, ACCESS_LEVEL_SUPER } = ACCESS_LEVELS;
 
 /* =================================================================================
+* 		[ Registra una nueva cuenta de usuario ]
+* ================================================================================= */
+
+server.post( '/signup', ( request, response, next ) => {
+	if ( request.isAuthenticated( ) ) {
+		return response.status( 400 ).send( { message: 'User is already logged in' } );
+	}
+	
+	const { firstName, lastName, email, password } = request.body;
+	
+	User.findOrCreate( {
+		where: {
+			email
+		},
+		defaults: {
+			firstName,
+			lastName,
+			password
+		}
+	} )
+	.then( ( [ user, created ] ) => {
+		if ( !created ) {
+			return response.status( 409 ).send( 'User already exists' );
+		}
+		
+		request.login( user, ( error ) => {
+			if ( error ) {
+				throw new Error( error );
+			}
+			
+			response.status( 200 ).send( user );
+		} );
+	} )
+	.catch( ( error ) => {
+		response.sendStatus( 500 );
+	} );
+} );
+
+/* =================================================================================
 * 		[ Identifica un usuario con su cuenta ]
 * ================================================================================= */
 
