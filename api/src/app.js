e5741ced59e5aac5379bccfcbc0bc3a34d2b86ec
manu96lp <manu96_lp@hotmail.com>
2020-10-23 10:46:56 -0300

    Import Express Session, Passport and some new environment variables

--- api/src/app.js
+++ api/src/app.js
@@ -1,15 +1,17 @@
-const express 		= require( 'express' );
-const session 		= require( 'express-session' );
-const fileUpload 	= require( 'express-fileupload' );
-const cookieParser 	= require( 'cookie-parser' );
-const bodyParser 	= require( 'body-parser' );
-const morgan 		= require( 'morgan' );
-const passport 		= require( 'passport' );
-const LocalStrategy = require( 'passport-local' ).Strategy;
-const routes 		= require( './routes/index.js' );
-const { User } 		= require( './db.js' );
+const express 			= require( 'express' );
+const session 			= require( 'express-session' );
+const fileUpload 		= require( 'express-fileupload' );
+const cookieParser 		= require( 'cookie-parser' );
+const bodyParser 		= require( 'body-parser' );
+const morgan 			= require( 'morgan' );
+const passport 			= require( 'passport' );
+const routes 			= require( './routes/index.js' );
 
+require( 'dotenv' ).config( );
 require( './db.js' );
+require( './passport.js' );
+
+const { FRONT_URL, SESSION_SECRET } = process.env;
 
 const server = express( );
 
@@ -20,56 +22,16 @@ server.use( fileUpload( ) );
 server.use( cookieParser( ) );
 server.use( bodyParser.urlencoded( { extended: true, limit: '50mb' } ) );
 server.use( bodyParser.json( { limit: '50mb' } ) );
-server.use( session( { secret: 'secret', resave: false, saveUninitialized: false } ) );
+server.use( session( { secret: SESSION_SECRET, resave: false, saveUninitialized: false } ) );
 server.use( morgan( 'dev' ) );
+
 server.use( ( request, response, next ) => {
-	response.header( 'Access-Control-Allow-Origin', 'http://localhost:3001' );
+	response.header( 'Access-Control-Allow-Origin', FRONT_URL );
 	response.header( 'Access-Control-Allow-Credentials', 'true' );
 	response.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
 	response.header( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE' );
 	
 	next( );
-} );
-
-passport.use( new LocalStrategy( {
-		usernameField: 'email',
-		passwordField: 'password'
-	},
-	function ( email, password, done ) {
-		User.findOne( {
-			where: { email }
-		} )
-		.then( ( user ) => {
-			if ( !user ) {
-				return done( null, false );
-			}
-			
-			if ( !user.correctPassword( password ) ) {
-				return done( null, false );
-			}
-			
-			console.log( 'logged' );
-			
-			return done( null, user );
-		} )
-		.catch( ( error ) => {
-			return done( error );
-		} );
-	}
-) );
-
-passport.serializeUser( function( user, done ) {
-	done( null, user.id );
-} );
-
-passport.deserializeUser( function( id, done ) {
-	User.findByPk( id )
-		.then( ( user ) => {
-			done( null, user );
-		} )
-		.catch( ( error ) => {
-			return done( error );
-		} );
 } );
 
 server.use( passport.initialize( ) );
