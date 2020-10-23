5501686469257c55f9dd1c52e52aa0973abf28f0
manu96lp <manu96_lp@hotmail.com>
2020-10-23 11:06:35 -0300

    Added login route (local strategy passport)

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,20 +1,16 @@
 const server = require( 'express' ).Router( );
 const passport = require( 'passport' );
-const { User } = require( '../db.js' );
 
-server.post( '/login', passport.authenticate( 'local', {
-	successMessage: 'Auth OK',
-	failureMessage: 'Error'
-} ), ( request, response, next ) => {
+/* =================================================================================
+* 		[ Identifica un usuario con su cuenta ]
+* ================================================================================= */
+
+server.post( '/login', passport.authenticate( 'local' ), ( request, response, next ) => {
 	response.send( request.user );
 } );
 
-server.get( '/test', ( request, response, next ) => {
-	if ( request.isAuthenticated( ) ) {
-		return response.sendStatus( 200 );
-	}
-	
-	return response.sendStatus( 401 );
-} );
+/* =================================================================================
+* 		[ Se exportan las rutas ]
+* ================================================================================= */
 
 module.exports = server;
