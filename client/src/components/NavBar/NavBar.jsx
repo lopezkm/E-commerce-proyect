fe87c14f7f2b886319996a8ddc753e06a81b7d40
Santiago <santiagojaviermartinez@gmail.com>
2020-10-24 17:13:49 -0300

    clickear logo redirecciona al home

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
-import { Nav, Navbar, Card, DropdownButton, ButtonGroup,Button } from 'react-bootstrap';
+import { Nav, Navbar, Card, DropdownButton, ButtonGroup, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
 import { ReactComponent as Logo } from '../../assets/logofull.svg';
@@ -15,7 +15,9 @@ function NavBar(props) {
 	return (
 		<Navbar collapseOnSelect expand="lg" fixed="top" variant="dark" className="navbar-main">
 			<Navbar.Brand>
-				<Logo className="navbar-logo" />
+				<Nav.Link as={Link} to="/">
+					<Logo className="navbar-logo" />
+				</Nav.Link>
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="navbarCollapse">
 				<FontAwesomeIcon icon={faBars} className="navbar-open-menu" />
@@ -23,66 +25,66 @@ function NavBar(props) {
 			</Navbar.Toggle>
 			<Navbar.Collapse id="navbarCollapse">
 				<Nav className="navbar-nav-left">
-					<NavLink as={ Link }  exact activeClassName= "active"  to="/products">
+					<NavLink as={Link} exact activeClassName="active" to="/products">
 						<p className="navbar-text navbar-text-outline">Tienda</p>
 					</NavLink>
 					<div className="navbar-separator"></div>
-					<NavLink as={ Link }  exact activeClassName="active" to="/admin">
+					<NavLink as={Link} exact activeClassName="active" to="/admin">
 						<p className="navbar-text navbar-text-outline">Administración</p>
 					</NavLink>
 				</Nav>
 				<Nav className="navbar-nav-right">
-					<NavLink as={ Link } exact activeClassName="active" to="/cart" className="navbar-nav-cart">
-						<FontAwesomeIcon icon={ faShoppingCart }/>
+					<NavLink as={Link} exact activeClassName="active" to="/cart" className="navbar-nav-cart">
+						<FontAwesomeIcon icon={faShoppingCart} />
 						<p className="navbar-text">
 							Carrito <span className="cart-count">{cartProductsCount && cartProductsCount}</span>
 						</p>
 					</NavLink>
-					{userFirstName ? 
-					<DropdownButton
-						as={ButtonGroup}
-						menuAlign={{ lg: 'right' }}
-						icon = {<FontAwesomeIcon icon={ faUser }/>}
-						title={
-							<p className="navbar-text">
-								{ userFirstName }
-							</p>}
-						id="dropdown-menu-align-responsive-1"
+					{userFirstName ?
+						<DropdownButton
+							as={ButtonGroup}
+							menuAlign={{ lg: 'right' }}
+							icon={<FontAwesomeIcon icon={faUser} />}
+							title={
+								<p className="navbar-text">
+									{userFirstName}
+								</p>}
+							id="dropdown-menu-align-responsive-1"
 						>
-						<Card>
-							<Card.Header bsPrefix="card-header">
-								<Card.Title bsPrefix="card-title">Hola  {/* {user.firstName} */} Matias</Card.Title>
-							</Card.Header>
-							
-							<Card.Body bsPrefix="card-body">
-								<Link to="/login/logued/shops">
-									<Card.Text bsPrefix="card-text">Mis Compras</Card.Text>
-								</Link>
-								<div>
-									<hr/>
-								</div>
-								<Link>
-									<Card.Text>Mis Datos</Card.Text>
-								</Link>
-								<div>
-									<hr/>
-								</div>
-								<Link>
-									<Card.Text>Seguridad</Card.Text>
-								</Link>
-							</Card.Body>
-							<Card.Footer>
-								<Button variant="ligth" className="card-button">Salir</Button>
-							</Card.Footer>
-						</Card>
-					</DropdownButton> :
-					<NavLink as={ Link } className="navbar-nav-user" exact activeClassName="active" to="/register" >
-						<FontAwesomeIcon icon={ faUser }/>
-						<p className="navbar-text">
-							{ userFirstName || 'Ingresar' }
-						</p>
-					</NavLink>}
-					<SearchBar/>
+							<Card>
+								<Card.Header bsPrefix="card-header">
+									<Card.Title bsPrefix="card-title">Hola  {/* {user.firstName} */} Matias</Card.Title>
+								</Card.Header>
+
+								<Card.Body bsPrefix="card-body">
+									<Link to="/login/logued/shops">
+										<Card.Text bsPrefix="card-text">Mis Compras</Card.Text>
+									</Link>
+									<div>
+										<hr />
+									</div>
+									<Link>
+										<Card.Text>Mis Datos</Card.Text>
+									</Link>
+									<div>
+										<hr />
+									</div>
+									<Link>
+										<Card.Text>Seguridad</Card.Text>
+									</Link>
+								</Card.Body>
+								<Card.Footer>
+									<Button variant="ligth" className="card-button">Salir</Button>
+								</Card.Footer>
+							</Card>
+						</DropdownButton> :
+						<NavLink as={Link} className="navbar-nav-user" exact activeClassName="active" to="/register" >
+							<FontAwesomeIcon icon={faUser} />
+							<p className="navbar-text">
+								{userFirstName || 'Ingresar'}
+							</p>
+						</NavLink>}
+					<SearchBar />
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
