126ab3e2985a6d01f63aed2265ef6d9f4a45c0f3
Matias Lopez Klyver <lopezk_matias@hotmail.com>
2020-11-01 01:36:13 -0300

    the status of the orders now change automatically from cart to processing when user goes to checkout, and from processing to completed or canceled regarding the result of the payment operation with Paypal. created status was not taking in count

--- client/src/components/Checkout/PayPal/Paypal.jsx
+++ client/src/components/Checkout/PayPal/Paypal.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ReactDOM from 'react-dom';
 import paypal from 'paypal-checkout';
 import { toast } from 'react-toastify';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const  PAYPAL_CLIENT_ID = 'AUupUI17YSCIIUg94t3uJGxU3ycZgIobjEeA82gt-EQ4VR_ikydiB8-h-Cm935yN3_2_93WbyAIW16EF'
 
 const PaypalCheckoutButton = ({ order }) => {
+
+    const userId = useSelector( ( state ) => state.user.id );
 
     const paypaConf = {
         currency: 'USD',
@@ -54,22 +60,45 @@ const PaypalCheckoutButton = ({ order }) => {
         .then(response => {
             //respuesta de paypal
             console.log(response);
-            alert('El pago se procesó correctamente, ID: ', response.id);
+            toast.success( 'El pago se procesó correctamente, ID: ', response.id, {
+				position: 'top-center',
+				autoClose: 2000,
+				hideProgressBar: true,
+				closeOnClick: true,
+				pauseOnHover: false,
+				draggable: true,
+				progress: undefined
+            } );
+           
+            return axios.get(`${API_URL}/users/${userId}/orders`, {withCredentials: true})
         })
+        .then(response =>  { 
+			let CartOrder = response.data.filter(order => order.status === 'processing');
+			return CartOrder;
+			
+		})
+	 	.then(CartOrder => {
+			return axios.put(`${API_URL}/orders/${CartOrder[0].id}`, {status: 'completed'}, {withCredentials: true})
+		})
+		.then(() => setTimeout(() => window.location.href= '/', 2010))
         .catch(error =>{
             console.log(error);
-            alert('Ocurrió un error al procesar el pago con PayPal');
+            toast.error( `¡Ocurrio un error al procesar el pago con Paypal!`, {
+                position: 'top-center',
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined
+            } );
+            setTimeout(() => window.location.href= '/checkout', 2010);
         });
     };
 
     const onError = (error) => {
         console.log(error);
-        alert('El pago no fue realizado, vuelva a intentarlo');
-    };
-
-    const onCancel = (data, actions) => {
-        console.log(data);
-        toast.error( `¡Compra cancelada!`, {
+        toast.error( `¡El pago no fue realizado, vuelva a intentarlo!`, {
             position: 'top-center',
             autoClose: 2000,
             hideProgressBar: true,
@@ -78,7 +107,29 @@ const PaypalCheckoutButton = ({ order }) => {
             draggable: true,
             progress: undefined
         } );
-        setTimeout(() => window.location.href= '/products', 2010);
+        setTimeout(() => window.location.href= '/checkout', 2010);
+    };
+
+    const onCancel = (data, actions) => {
+            console.log(data)
+                toast.error( `¡Compra cancelada!`, {
+                position: 'top-center',
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined
+            } );
+            return axios.get(`${API_URL}/users/${userId}/orders`, {withCredentials: true})
+        .then(response =>  { console.log('estoy en la respuesta de axios')
+			let CartOrder = response.data.filter(order => order.status === 'processing');
+			return CartOrder;
+		})
+		.then(CartOrder => { console.log('filtre la orden')
+			return axios.put(`${API_URL}/orders/${CartOrder[0].id}`, {status: 'canceled'}, {withCredentials: true})
+		})
+		.then((res) => console.log('termine', res.status), setTimeout(() => window.location.href= '/', 2010)) 
     };
 
     return(
